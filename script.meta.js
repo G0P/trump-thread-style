a210a5528639cbc972f4a4a9950f6a0653542856
G0P <as_if@gmail.com>
2016-05-28 14:51:38 +0200

    updated version

--- script.meta.js
+++ script.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Trump Thread Style
-// @version      1.1.4
+// @version      1.1.5
 // @namespace    TTS
 // @description  MAGA theme for trump general threads on 4chan
 // @license      nignog license
